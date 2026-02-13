c6164426ff9cfa1a556d4a69ec2f982ea19c3adf
Andrii Luchko <andriiluchko@arounda.agency>
2026-02-13 23:30:18 +0200

    feat: Add unit tests for TimerCubit, UserCubit, and GenericValidationCubit with mock dependencies

---
+++ test/core/user/mocks/mock_user_repository.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:reforge/core/user/domain/repositories/user_repository.dart';
+
+class MockUserRepository extends Mock implements UserRepository {}
