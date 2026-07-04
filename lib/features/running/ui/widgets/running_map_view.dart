bc8ec72d7fe4a27ed5997b43e273155c7544e3dc
Andrii Luchko <a.luchko.dev@gmail.com>
2026-07-04 04:21:21 +0300

    feat: Add dark map style for improved user experience in running map view

--- lib/features/running/ui/widgets/running_map_view.dart
+++ lib/features/running/ui/widgets/running_map_view.dart
@@ -3,11 +3,43 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:reforge/app/theme/app_theme.dart';
+import 'package:reforge/features/running/constants/map_styles.dart';
 import 'package:reforge/features/running/constants/running_constants.dart';
 import 'package:reforge/features/running/domain/entities/route_coordinate.dart';
-import 'package:reforge/features/running/ui/widgets/map_styles.dart';
 import 'package:reforge/generated/flutter_gen/assets.gen.dart';
 import 'package:reforge/shared/uikit/buttons/icon_button.dart';
+
+@immutable
+class InterpolatedPosition {
+  const InterpolatedPosition({
+    required this.latitude,
+    required this.longitude,
+    required this.heading,
+  });
+
+  final double latitude;
+  final double longitude;
+  final double heading;
+}
+
+InterpolatedPosition interpolateRoutePosition({
+  required RouteCoordinate from,
+  required RouteCoordinate to,
+  required double t,
+}) {
+  final lat = from.latitude + (to.latitude - from.latitude) * t;
+  final lng = from.longitude + (to.longitude - from.longitude) * t;
+
+  var headingDiff = (to.heading - from.heading) % 360.0;
+  if (headingDiff > 180.0) {
+    headingDiff -= 360.0;
+  } else if (headingDiff < -180.0) {
+    headingDiff += 360.0;
+  }
+  final heading = (from.heading + headingDiff * t) % 360.0;
+
+  return InterpolatedPosition(latitude: lat, longitude: lng, heading: heading);
+}
 
 class RunningMapView extends StatefulWidget {
   const RunningMapView({
@@ -25,7 +57,7 @@ class _RunningMapViewState extends State<RunningMapView> with SingleTickerProvid
   GoogleMapController? _controller;
   BitmapDescriptor? _userLocationIcon;
 
-  late AnimationController _markerAnimController;
+  late final AnimationController _markerAnimController;
   RouteCoordinate? _oldPosition;
   RouteCoordinate? _targetPosition;
 
@@ -42,21 +74,68 @@ class _RunningMapViewState extends State<RunningMapView> with SingleTickerProvid
     _markerAnimController = AnimationController(
       vsync: this,
       duration: RunningConstants.engineTickInterval,
-    )..addListener(() {
-          setState(() {});
-        });
+    )..addListener(_onMarkerAnimTick);
 
     if (widget.routeMap.isNotEmpty) {
       _oldPosition = widget.routeMap.last;
       _targetPosition = widget.routeMap.last;
-      _rebuildPolyline();
     }
   }
 
   @override
   void dispose() {
-    _markerAnimController.dispose();
+    _markerAnimController
+      ..removeListener(_onMarkerAnimTick)
+      ..dispose();
     super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(RunningMapView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    final hasNewPoints = widget.routeMap.length != oldWidget.routeMap.length;
+    if (widget.routeMap.isEmpty) return;
+
+    if (hasNewPoints) {
+      _rebuildPolyline();
+    }
+
+    _handleLatestPositionChanged();
+
+    if (_followUser && hasNewPoints) {
+      _animateToLatest();
+    }
+  }
+
+  void _onMarkerAnimTick() => setState(() {});
+
+  void _rebuildPolyline() {
+    _polylines = {
+      Polyline(
+        polylineId: const PolylineId('route'),
+        points: widget.routeMap.map((p) => LatLng(p.latitude, p.longitude)).toList(),
+        color: context.appTheme.orange100,
+        width: 12,
+        jointType: JointType.round,
+        startCap: Cap.roundCap,
+        endCap: Cap.roundCap,
+      ),
+    };
+  }
+
+  void _handleLatestPositionChanged() {
+    final latest = widget.routeMap.last;
+    final isSamePosition =
+        _targetPosition != null &&
+        _targetPosition!.latitude == latest.latitude &&
+        _targetPosition!.longitude == latest.longitude;
+
+    if (isSamePosition) return;
+
+    _oldPosition = _targetPosition ?? latest;
+    _targetPosition = latest;
+    _markerAnimController.forward(from: 0);
   }
 
   Future<void> _initCustomMarker() async {
@@ -69,57 +148,6 @@ class _RunningMapViewState extends State<RunningMapView> with SingleTickerProvid
       setState(() {
         _userLocationIcon = icon;
       });
-    }
-  }
-
-  void _rebuildPolyline() {
-    final appTheme = context.appTheme;
-    _polylines = {
-      Polyline(
-        polylineId: const PolylineId('route'),
-        points: widget.routeMap.map((p) => LatLng(p.latitude, p.longitude)).toList(),
-        color: appTheme.orange100,
-        width: 12,
-        jointType: JointType.round,
-        startCap: Cap.roundCap,
-        endCap: Cap.roundCap,
-      ),
-    };
-  }
-
-  @override
-  void didUpdateWidget(RunningMapView oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    final hasChanged = widget.routeMap.length != oldWidget.routeMap.length;
-
-    if (hasChanged) {
-      _rebuildPolyline();
-    }
-
-    if (widget.routeMap.isNotEmpty) {
-      _handleNewPosition(widget.routeMap.last);
-    }
-
-    if (hasChanged) {
-      _maybeFollowCamera();
-    }
-  }
-
-  void _handleNewPosition(RouteCoordinate latest) {
-    if (_targetPosition == null ||
-        _targetPosition!.latitude != latest.latitude ||
-        _targetPosition!.longitude != latest.longitude) {
-      _oldPosition = _targetPosition ?? latest;
-      _targetPosition = latest;
-
-      _markerAnimController.forward(from: 0);
-    }
-  }
-
-  void _maybeFollowCamera() {
-    if (_followUser && widget.routeMap.isNotEmpty) {
-      _animateToLatest();
     }
   }
 
@@ -152,6 +180,13 @@ class _RunningMapViewState extends State<RunningMapView> with SingleTickerProvid
     }
   }
 
+  void _handleRecenterPressed() {
+    setState(() {
+      _followUser = true;
+    });
+    _animateToLatest();
+  }
+
   @override
   Widget build(BuildContext context) {
     final appTheme = context.appTheme;
@@ -165,24 +200,23 @@ class _RunningMapViewState extends State<RunningMapView> with SingleTickerProvid
       );
     }
 
-    _InterpolatedPosition interpolated;
-
-    if (_oldPosition != null && _targetPosition != null) {
-      interpolated = _interpolate(_oldPosition!, _targetPosition!, _markerAnimController.value);
-    } else {
-      final currentPosition = widget.routeMap.last;
-      interpolated = _InterpolatedPosition(
-        currentPosition.latitude,
-        currentPosition.longitude,
-        currentPosition.heading,
-      );
-    }
+    final position = _oldPosition != null && _targetPosition != null
+        ? interpolateRoutePosition(
+            from: _oldPosition!,
+            to: _targetPosition!,
+            t: _markerAnimController.value,
+          )
+        : InterpolatedPosition(
+            latitude: widget.routeMap.last.latitude,
+            longitude: widget.routeMap.last.longitude,
+            heading: widget.routeMap.last.heading,
+          );
 
     final userMarker = Marker(
       markerId: const MarkerId('user_current_location'),
-      position: LatLng(interpolated.lat, interpolated.lng),
+      position: LatLng(position.latitude, position.longitude),
       icon: _userLocationIcon ?? BitmapDescriptor.defaultMarker,
-      rotation: interpolated.heading,
+      rotation: position.heading,
       flat: true,
       anchor: const Offset(0.5, 0.5),
     );
@@ -191,7 +225,7 @@ class _RunningMapViewState extends State<RunningMapView> with SingleTickerProvid
       children: [
         GoogleMap(
           initialCameraPosition: CameraPosition(
-            target: LatLng(interpolated.lat, interpolated.lng),
+            target: LatLng(position.latitude, position.longitude),
             zoom: RunningConstants.mapCameraZoomActive,
           ),
           polylines: _polylines,
@@ -205,41 +239,18 @@ class _RunningMapViewState extends State<RunningMapView> with SingleTickerProvid
           onMapCreated: (controller) {
             _controller = controller;
           },
-
           onCameraMoveStarted: _handleCameraMoveStarted,
         ),
-
         if (!_followUser)
           Positioned(
             bottom: 20,
             right: 16,
             child: AppIconButton(
               iconAsset: Assets.images.icons.myLocation,
-              onPressed: () {
-                setState(() {
-                  _followUser = true;
-                });
-                _animateToLatest();
-              },
+              onPressed: _handleRecenterPressed,
             ),
           ),
       ],
     );
   }
-}
-
-class _InterpolatedPosition {
-  const _InterpolatedPosition(this.lat, this.lng, this.heading);
-  final double lat;
-  final double lng;
-  final double heading;
-}
-
-_InterpolatedPosition _interpolate(RouteCoordinate from, RouteCoordinate to, double t) {
-  final lat = from.latitude + (to.latitude - from.latitude) * t;
-  final lng = from.longitude + (to.longitude - from.longitude) * t;
-  var diff = (to.heading - from.heading) % 360.0;
-  if (diff > 180.0) diff -= 360.0;
-  if (diff < -180.0) diff += 360.0;
-  return _InterpolatedPosition(lat, lng, (from.heading + diff * t) % 360.0);
 }
