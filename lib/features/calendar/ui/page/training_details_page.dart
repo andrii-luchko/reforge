a13e95a6ad5d147edff66056c5fc3c82f091e994
Andrii Luchko <a.luchko.dev@gmail.com>
2026-07-07 02:45:45 +0300

    feat: Update exercise results display with imperial conversion and add empty state handling for training details

--- lib/features/calendar/ui/page/training_details_page.dart
+++ lib/features/calendar/ui/page/training_details_page.dart
@@ -13,6 +13,7 @@ import 'package:reforge/features/quiz/domain/enums/measure_system.dart';
 import 'package:reforge/features/workout_common/domain/entities/previous_exercise_result.dart';
 import 'package:reforge/generated/i18n/translations.g.dart';
 import 'package:reforge/shared/default_sliver_app_bar.dart';
+import 'package:reforge/shared/empty_list_message.dart';
 import 'package:reforge/shared/uikit/default_background.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
@@ -191,7 +192,7 @@ class _TrainingDetailsContentView extends StatelessWidget {
               padding: TrainingDetailsBody.horizontalPadding.copyWith(bottom: 16),
               sliver: SliverToBoxAdapter(
                 child: XpTile(
-                  progress: 0.5,
+                  progress: data.totalXpEarned == 0 ? 0 : 0.5,
                   xp: data.totalXpEarned,
                 ).animateEntrance(),
               ),
@@ -207,18 +208,25 @@ class _TrainingDetailsContentView extends StatelessWidget {
             ),
             SliverPadding(
               padding: TrainingDetailsBody.horizontalPadding.copyWith(bottom: 16),
-              sliver: SliverList.separated(
-                itemCount: data.exercises.length,
-                itemBuilder: (context, index) {
-                  return WorkoutInfoTile(
-                    result: data.exercises[index],
-                    system: data.measurementSystem,
-                  ).animateEntrance();
-                },
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: 16,
-                ),
-              ),
+              sliver: data.exercises.isEmpty
+                  //TODO(Masayoshi): translate
+                  ? SliverEmptyListMessage(
+                      icon: Icons.auto_stories_outlined,
+                      title: 'No Info Found',
+                      subtitle: 'It seems there are no results available at the moment. Please check back later.',
+                    )
+                  : SliverList.separated(
+                      itemCount: data.exercises.length,
+                      itemBuilder: (context, index) {
+                        return WorkoutInfoTile(
+                          result: data.exercises[index],
+                          system: data.measurementSystem,
+                        ).animateEntrance();
+                      },
+                      separatorBuilder: (context, index) => const SizedBox(
+                        height: 16,
+                      ),
+                    ),
             ),
           ],
         ),
