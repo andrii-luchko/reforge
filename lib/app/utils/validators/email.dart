cf61a1fc6b8e915372285298f82a25b46b76b3af
Andrii Luchko <a.luchko.dev@gmail.com>
2026-07-14 07:25:47 +0300

    feat: refactor email validation regex for improved readability and maintainability

--- lib/app/utils/validators/email.dart
+++ lib/app/utils/validators/email.dart
@@ -1,11 +1,21 @@
+// ignore_for_file: unnecessary_raw_strings, prefer_single_quotes
+
 import 'package:reforge/generated/i18n/translations.g.dart';
+
+final _emailRegex = RegExp(
+  r"^[A-Za-z0-9.!#$%&'*+/=?^_`{|}~-]+"
+  r"@[A-Za-z0-9]"
+  r"(?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?"
+  r"(?:\.[A-Za-z0-9]"
+  r"(?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)+$",
+);
 
 String? validateEmail(String? value) {
   if (value == null || value.trim().isEmpty) {
     return t.validation.email_required;
   }
 
-  if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
+  if (!_emailRegex.hasMatch(value)) {
     return t.validation.email_invalid;
   }
 
