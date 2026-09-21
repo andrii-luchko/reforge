aad4eb6b9d543fd9f39fd61fa5d6e97ed2eaa028
Andrii Luchko <a.luchko.dev@gmail.com>
2026-09-21 14:31:38 +0300

    chore: Update target SDK version to 36 and increment build number. builds 0.37.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
     defaultConfig {
         applicationId = "com.reforgestudios.reforge"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
